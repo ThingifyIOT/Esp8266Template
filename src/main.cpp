391ad2e01e430e1b394a540b3436dd3913eb0b31
Tomasz Scislowicz <toomasz@gmail.com>
2019-11-10 19:05:45 +0100

    add example how to add nodes

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,51 @@
 #include <ContiEsp.h>
 #include <DebugNodesModule.h>
 
-ContiEsp thing("<enter token>", "Sample Thing");
+ContiEsp thing("<token>", "Sample Thing");
 
-bool OnRelayValueChanged(void*_, Node *node)
+
+bool OnBoolChanged(void*_, Node *node)
 {
+  bool value = node->Value.AsBool();
+  if(value)
+  {
+    Serial.printf("Bool node changed to true");
+  }
+  else
+  {
+    Serial.printf("Bool node changed to false");
+  }
+  
+
 	return true;
 }
+
+bool OnNodeValueChanged(void*_, Node *node)
+{
+  Serial.printf("'%s' node changed value to '%s'\n", node->name(), node->Value.toString().c_str());
+	return true;
+}
+
+NodeValue RapidIgnitionFired(void* context, FunctionArguments &args)
+{
+	return NodeValue::String("Ignited");
+}
+
 
 void setup()
 {
 	Serial.begin(500000);
-
-	thing.AddAp("<wifi_name>", "<password>");
 
 	auto debugNodes = new DebugNodesModule(thing);
 	debugNodes->UpdateIntervalInMs = 1000;
 	thing.AddModule(debugNodes);
 	thing.WatchdogEnabled = true;
 
+  thing.AddBoolean("bool")->OnChanged(OnNodeValueChanged);
+  thing.AddFunction("Function", RapidIgnitionFired);
+  thing.AddInt("integer", ContiUnit::Volt)->SetValue(NodeValue::Int(220));
+  thing.AddRange("range", 10, 30, 2, ContiUnit::Percent)->OnChanged(OnNodeValueChanged);
+  thing.AddColor("Color")->OnChanged(OnNodeValueChanged);
 	thing.Start();
 }
 
